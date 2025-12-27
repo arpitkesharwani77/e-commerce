8e621706c6edc52caf03e953177de427dceea842
arpitkesharwani77 <arpitkesharwani77@gmail.com>
2025-12-28 01:51:04 +0530

    collection page : filter & sort

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        <Link to="/cart" className=" relative cursor-pointer bg-red-500">
+        <Link to="/cart" className=" relative cursor-pointer">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="cart icon" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4  bg-black text-white aspect-square rounded-full text-[8px]  ">
             10
